f3af3232d9c81e3ba39b3411599f247207f8d805
Brayden <humortronic@gmail.com>
2023-07-16 18:03:24 +0930

    Added tkinter window to telemetry.py

--- telemetry.py
+++ telemetry.py
@@ -1,1 +1,13 @@
-#Telemetry User-Interface for ELA internship
+# Telemetry User-Interface for ELA internship
+# Started 16-07-2023
+
+import tkinter as tk
+from PIL import ImageTk, Image
+
+window = tk.Tk()
+window.title("Simulated Telemetry UI")
+window.geometry("1920x1080")
+window.resizable(False, False)
+window.configure(background="black")
+
+window.mainloop()
