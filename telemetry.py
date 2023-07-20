3d563e78fd6d1a4b18d66fe90a5d4898ce7c2e97
Brayden <humortronic@gmail.com>
2023-07-20 17:20:01 +0930

    Added changes to file

--- telemetry.py
+++ telemetry.py
@@ -3,14 +3,21 @@
 
 import tkinter as tk
 from PIL import ImageTk, Image
+import random
+import threading
+import time
 
+class   TelemetryInterface:
+    def __init__(self, window):
+        self.window = window
+        self.window.title("Simulated Telemetry UI")
+        self.window.geometry("1920x1080")
+        self.window.resizable(False, False)
+        self.window.configure(background="black")
+
+        # Telemetry Data Labels
+        
 
 window = tk.Tk()
-window.title("Simulated Telemetry UI")
-window.geometry("1920x1080")
-window.resizable(False, False)
-window.configure(background="black")
-
 print("WINDOW STARTED")
-
 window.mainloop()
