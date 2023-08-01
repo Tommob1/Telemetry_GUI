aec4b0515e5a62e6350de7357c74d54c6ffe20e0
Brayden Tomlinson <humortronic@gmail.com>
2023-08-01 16:41:20 +0930

    Modified file

--- telemetry.py
+++ telemetry.py
@@ -18,21 +18,43 @@ class TelemetryInterface:
 
         # Display logo
         self.logo()
+        self.window.after(5000, self.init_simulation)
+
+    def logo(self):
+        # Get the directory of this script
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+    
+        logo_path = os.path.join(script_dir, 'ela_logo.png')
+        logo = Image.open(logo_path)
+
+        width = 481
+        height = 312
+
+        logo = logo.resize((width, height), Image.LANCZOS)
+        logo_image = ImageTk.PhotoImage(logo)
+
+        self.logo_label = tk.Label(self.window, image=logo_image, borderwidth=0, bg="black")
+        self.logo_label.image = logo_image
+        self.logo_label.pack()
+
+    def init_simulation(self):
+        # Remove logo
+        self.logo_label.pack_forget()
 
         # Telemetry Data Labels
-        self.altitude_label = tk.Label(window, fg="#00FF00", bg="black", font=("Courier", 20))
+        self.altitude_label = tk.Label(self.window, fg="#00FF00", bg="black", font=("Courier", 20))
         self.altitude_label.pack()
 
-        self.velocity_label = tk.Label(window, fg="#00FF00", bg="black", font=("Courier", 20))
+        self.velocity_label = tk.Label(self.window, fg="#00FF00", bg="black", font=("Courier", 20))
         self.velocity_label.pack()
 
-        self.fuel_label1 = tk.Label(window, fg="#00FF00", bg="black", font=("Courier", 20))
+        self.fuel_label1 = tk.Label(self.window, fg="#00FF00", bg="black", font=("Courier", 20))
         self.fuel_label1.pack()
 
-        self.fuel_label2 = tk.Label(window, fg="#00FF00", bg="black", font=("Courier", 20))
+        self.fuel_label2 = tk.Label(self.window, fg="#00FF00", bg="black", font=("Courier", 20))
         self.fuel_label2.pack()
 
-        self.staging_label = tk.Label(window, fg="#00FF00", bg="black", font=("Courier", 23))
+        self.staging_label = tk.Label(self.window, fg="#00FF00", bg="black", font=("Courier", 23))
         self.staging_label.pack()
 
         # Telemetry Data Graphs
@@ -56,7 +78,7 @@ class TelemetryInterface:
         self.fig.subplots_adjust(hspace=0.5)
         self.fig.patch.set_facecolor('black')
 
-        self.canvas = FigureCanvasTkAgg(self.fig, master=window)  
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.window)  
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
 
@@ -68,23 +90,6 @@ class TelemetryInterface:
 
         # Update function launched in a new thread
         threading.Thread(target=self.update_telemetry_data).start()
-
-    def logo(self):
-        # Get the directory of this script
-        script_dir = os.path.dirname(os.path.realpath(__file__))
-    
-        logo_path = os.path.join(script_dir, 'ela_logo.png')
-        logo = Image.open(logo_path)
-
-        width = 481
-        height = 312
-
-        logo = logo.resize((width, height), Image.LANCZOS)
-        logo_image = ImageTk.PhotoImage(logo)
-
-        self.logo_label = tk.Label(self.window, image=logo_image, borderwidth=0, bg="black")
-        self.logo_label.image = logo_image
-        self.logo_label.pack()
 
     def update_telemetry_data(self):
         simulation_time = time.time()
@@ -100,41 +105,46 @@ class TelemetryInterface:
             velocity = 5000 * (1 - math.exp(-velocity_growth_factor * elapsed_time))  # Velocity increases from 0 to 5000
             fuel = 100 - (100 * elapsed_percent)  # Fuel decreases from 100% to 0%
 
-            # Update the labels with the new data
-            self.altitude_label.config(text="Altitude: %.2f KM" % altitude)
-            self.velocity_label.config(text="Velocity: %.2f KM/H" % velocity)
-            self.fuel_label1.config(text="Stage 1 Fuel: %.2f%%" % fuel)
-            self.fuel_label2.config(text="Stage 2 Fuel: 100%")
-
             # Append the new data to the lists
             self.times.append(elapsed_time)
             self.altitudes.append(altitude)
             self.velocities.append(velocity)
             self.fuels.append(fuel)
 
-            # Plot the new data
-            self.altitude_graph.clear()
-            self.altitude_graph.plot(self.times, self.altitudes, 'g')
-            self.altitude_graph.set_xlim(0, 60)
-            self.altitude_graph.set_ylim(0, 100)
-
-            self.velocity_graph.clear()
-            self.velocity_graph.plot(self.times, self.velocities, 'g')
-            self.velocity_graph.set_xlim(0, 60)
-            self.velocity_graph.set_ylim(0, 5000)
-
-            self.fuel_graph.clear()
-            self.fuel_graph.plot(self.times, self.fuels, 'g')
-            self.fuel_graph.set_xlim(0, 60)
-            self.fuel_graph.set_ylim(0, 100)
-
-            self.canvas.draw()
+            # Schedule the UI update in the main thread
+            self.window.after(0, self.update_ui, altitude, velocity, fuel)
 
             # Wait for 0.1 second before updating again
-            time.sleep(0.01)
+            time.sleep(0.1)
 
+        self.window.after(0, self.staging_label.config, {"text":"STAGE SEPERATION CONFIRMED"})
         print("SIMULATION COMPLETE")
-        self.staging_label.config(text="STAGE SEPERATION CONFIRMED")
+
+    def update_ui(self, altitude, velocity, fuel):
+        # Update the labels with the new data
+        self.altitude_label.config(text="Altitude: %.2f KM" % altitude)
+        self.velocity_label.config(text="Velocity: %.2f KM/H" % velocity)
+        self.fuel_label1.config(text="Stage 1 Fuel: %.2f%%" % fuel)
+        self.fuel_label2.config(text="Stage 2 Fuel: 100%")
+
+        # Plot the new data
+        self.altitude_graph.clear()
+        self.altitude_graph.plot(self.times, self.altitudes, 'g')
+        self.altitude_graph.set_xlim(0, 60)
+        self.altitude_graph.set_ylim(0, 100)
+
+        self.velocity_graph.clear()
+        self.velocity_graph.plot(self.times, self.velocities, 'g')
+        self.velocity_graph.set_xlim(0, 60)
+        self.velocity_graph.set_ylim(0, 5000)
+
+        self.fuel_graph.clear()
+        self.fuel_graph.plot(self.times, self.fuels, 'g')
+        self.fuel_graph.set_xlim(0, 60)
+        self.fuel_graph.set_ylim(0, 100)
+
+        self.canvas.draw()
+
 
 window = tk.Tk()
 print("WINDOW STARTED")
